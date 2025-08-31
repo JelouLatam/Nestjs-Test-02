40f4b38184eab7c8c917996fe8fc53229834ad6e
witlac <cjhairmejia@gmail.com>
2025-08-30 22:06:17 -0500

    feat(cache): add caching to most accessed task endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,22 @@ import { TaskController } from './modules/tasks/presentation/task.controller';
 import { TaskService } from './modules/tasks/application/task.service';
 import { Task } from './modules/tasks/domain/task.entity';
 import { LoggerMiddleware } from './shared/logger.middleware';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      useFactory: async () => {
+        return {
+          stores: [
+            createKeyv(`redis://${process.env.REDIS_HOST || 'redis'}:${process.env.REDIS_PORT || 6379}`),
+          ],
+          ttl: 10000,
+        };
+      },
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
